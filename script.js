3de881b60279211c22a7318a418e552524c0e3dc
something-natural <tsglow@naver.com>
2024-09-09 15:33:57 +0900

    feat: added vars

---
+++ script.js
@@ -0,0 +1,16 @@
+//buttons
+const addTaskBtn = document.getElementById("open-task-form-btn");
+const closeBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateBtn = document.getElementById("add-or-update-task-btn"); // you should change inner text too
+const cancelBtn = document.getElementById("cancel-btn");
+const discardBtn = document.getElementById("discard-btn");
+
+//inputs
+const inputTaskTitle = document.getElementById("title-input");
+const inputTaskDate = document.getElementById("date-input");
+const inputTaskDescription = document.getElementById("description-input");
+
+//tags change inner html later
+const taskContainer = document.getElementById("tasks-container")
+
+
